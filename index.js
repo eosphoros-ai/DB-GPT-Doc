337a05d409ecdcfb61fb741702ef02920399bad3
licunxing <864255598@qq.com>
2023-12-21 00:17:51 +0800

    feat: add auto deploy

---
+++ index.js
@@ -0,0 +1,26 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const { createProjectAction } = require("./lib/core/action");
+const initProject = require("./lib/core/initProject");
+
+require("dotenv").config();
+
+const app = new Koa();
+var router = new Router();
+
+router.get("/deploy", async (ctx, next) => {
+  await createProjectAction();
+  ctx.body = "GET Success!";
+});
+
+router.post("/deploy", async (ctx, next) => {
+  await createProjectAction();
+  ctx.body = "POST Success!";
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(process.env.APP_PORT, () => {
+  initProject();
+  console.log(`Service started successfully: http://localhost:${process.env.APP_PORT}`, );
+});
