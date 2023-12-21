7daa2b783a0dcb81327f5353c8332b95eb582427
licunxing <864255598@qq.com>
2023-12-22 00:29:29 +0800

    feat: test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Koa = require("koa");
 const Router = require("koa-router");
 const { createProjectAction } = require("./lib/core/action");
 const initProject = require("./lib/core/initProject");
+const { publishNewVersion } = require("./lib/core/action");
 
 require("dotenv").config();
 
@@ -15,6 +16,16 @@ router.get("/deploy", async (ctx, next) => {
 
 router.post("/deploy", async (ctx, next) => {
   await createProjectAction();
+  ctx.body = "POST Success!";
+});
+
+router.get("/publish", async (ctx, next) => {
+  await publishNewVersion();
+  ctx.body = "GET Success!";
+});
+
+router.post("/publish", async (ctx, next) => {
+  await publishNewVersion();
   ctx.body = "POST Success!";
 });
 
