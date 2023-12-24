c881bbe05d03b58f3eb86e50aa8078345599d0cb
licunxing <864255598@qq.com>
2023-12-24 15:37:44 +0800

    feat: test tag

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ const app = new Koa();
 var router = new Router();
 
 router.get("/deploy", async (ctx, next) => {
-  await createProjectAction();
-  ctx.body = "GET Success!";
+  console.log('========', ctx.request.body);
+  const tag = ctx.request.body.tag;
+  await createProjectAction(tag);
+  ctx.body = "GET Publish Success!";
 });
 
 router.post("/deploy", async (ctx, next) => {
-  await createProjectAction();
-  ctx.body = "POST Success!";
+  console.log('========', ctx.request.body);
+  const tag = ctx.request.body.tag;
+  await createProjectAction(tag);
+  ctx.body = "POST Publish Success!";
 });
 
 router.get("/publish", async (ctx, next) => {
